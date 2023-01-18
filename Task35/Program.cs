5dcfef33d7ad68aff9059f4e602284c070980e03
Artem <grekartemvl@mail.ru>
2023-01-18 14:15:58 +0500

    Add Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 35. Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в отрезке [10, 99].
+*/
+int[] generateArray(int length, int start, int end)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+int getIntFromUser(string message)
+{
+    Console.WriteLine(message);
+    int userInt = int.Parse(Console.ReadLine()!);
+    return userInt;
+}
+int Find(int[] array, int start, int end)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= start && array[i] <= end)
+        {
+            count += 1;
+                }
+    }
+    return count;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int number = getIntFromUser("Введите количество элементов массива");
+int[] array = generateArray(number, -100, 100);
+printArray(array);
+int start = getIntFromUser("Введите начальное число диапазона");
+int end = getIntFromUser("Введите конечное число диапазона");
+int count = Find(array, start, end);
+Console.WriteLine($"Количество элементов в диапазоне [{start}, {end}] = {count}");
